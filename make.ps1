f6fd91cd4cd94f4391ccb1110e0c64f15cba5d63
Jeyko <jakeactually@outlook.com>
2019-02-27 15:43:30 -0500

    fix for powershell encoding

--- make.ps1
+++ make.ps1
@@ -1,4 +1,5 @@
 elm make src\FileManager.elm --optimize --output src\output.js
 $args = "pure_funcs='F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9',pure_getters,keep_fargs=false,unsafe_comps,unsafe"
-uglifyjs src\output.js src\wrapper.js -c $args | uglifyjs -m -o dist\file-manager.js
+uglifyjs src\output.js src\wrapper.js -c $args -o dist\file-manager.js
+uglifyjs dist\file-manager.js -m -o dist\file-manager.js
 cp dist\* ..\elm-file-manager-demo\static
