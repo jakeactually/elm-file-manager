0403c92475759eaf9bbc237a4e8912794706dcc0
Jeyko <jakeactually@outlook.com>
2019-02-27 11:09:35 -0500

    wrapper to es5, improved uglify, awesome size!

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,94 +1,100 @@
-const FileManager = (() => {
-    const renderDrop = () => {
-        const observer = new MutationObserver(() => {
-            const files = document.querySelector('.fm-files');
-            if (files) {
-                files.ondrop = ev => {
-                    const dir = document.querySelector('.fm-text').innerText;                
-                    const files = Array.from(ev.dataTransfer.files);
-                    fm.ports.onFilesAmount.send(files.length);
-                    uploadFiles(dir, files);
-                };
-                observer.disconnect();
-            }
-        });
-        observer.observe(document.body, { childList: true, subtree: true });
-    };
+var FileManager = function () {
+  var renderDrop = function renderDrop() {
+    var observer = new MutationObserver(function () {
+      var files = document.querySelector('.fm-files');
 
-    const upload = dir => {
-        const input = document.querySelector('.fm-file-input');
-        const files = Array.from(input.files);
-        fm.ports.onFilesAmount.send(files.length);
-        uploadFiles(dir, files);
-    };
-
-    const uploadFiles = (dir, files) => {
-        if (files.length == 0) {
-            return;
-        }
-
-        const file = files.shift();
-
-        const formData = new FormData();
-        formData.append('dir', dir);
-        formData.append('file', file);
-
-        const xhr = new XMLHttpRequest();
-        xhr.upload.onprogress = ev => {
-            if (ev.lengthComputable) {
-                fm.ports.onProgress.send(Math.floor(ev.loaded * 100 / ev.total));
-            }
+      if (files) {
+        files.ondrop = function (ev) {
+          var dir = document.querySelector('.fm-text').innerText;
+          var files = Array.from(ev.dataTransfer.files);
+          fm.ports.onFilesAmount.send(files.length);
+          uploadFiles(dir, files);
         };
-        xhr.onload = () => {
-            fm.ports.onUploaded.send(null);
-            uploadFiles(dir, files);
-        };
-        xhr.open('POST', uploadsUrl);
-        xhr.send(formData);
+
+        observer.disconnect();
+      }
+    });
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    });
+  };
+
+  var upload = function upload(dir) {
+    var input = document.querySelector('.fm-file-input');
+    var files = Array.from(input.files);
+    fm.ports.onFilesAmount.send(files.length);
+    uploadFiles(dir, files);
+  };
+
+  var uploadFiles = function uploadFiles(dir, files) {
+    if (files.length == 0) {
+      return;
+    }
+
+    var file = files.shift();
+    var formData = new FormData();
+    formData.append('dir', dir);
+    formData.append('file', file);
+    var xhr = new XMLHttpRequest();
+
+    xhr.upload.onprogress = function (ev) {
+      if (ev.lengthComputable) {
+        fm.ports.onProgress.send(Math.floor(ev.loaded * 100 / ev.total));
+      }
     };
 
-    const download = files => {
-        files.forEach(file => window.open(downloadsUrl + file));
+    xhr.onload = function () {
+      fm.ports.onUploaded.send(null);
+      uploadFiles(dir, files);
     };
 
-    let uploadsUrl;
-    let downloadsUrl;
-    let fm;
+    xhr.open('POST', uploadsUrl);
+    xhr.send(formData);
+  };
 
-    return options => {
-        renderDrop();
+  var download = function download(files) {
+    files.forEach(function (file) {
+      return window.open(downloadsUrl + file);
+    });
+  };
 
-        uploadsUrl = options.uploadsUrl;
-        downloadsUrl = options.downloadsUrl;
+  var uploadsUrl;
+  var downloadsUrl;
+  var fm;
+  return function (options) {
+    renderDrop();
+    uploadsUrl = options.uploadsUrl;
+    downloadsUrl = options.downloadsUrl;
+    var container;
 
-        let container;
-        if (!options.container) {
-            container = document.createElement('div');
-            document.body.appendChild(container);
-        } else {
-            container = options.container;
-        }
+    if (!options.container) {
+      container = document.createElement('div');
+      document.body.appendChild(container);
+    } else {
+      container = options.container;
+    }
 
-        fm = Elm.FileManager.init({
-            node: container,
-            flags: {
-                api: options.api,
-                thumbnailsUrl: options.thumbnailsUrl,             
-                jwtToken: options.jwtToken || "",
-                dir: options.dir || "/"
-            }
-        });
+    fm = Elm.FileManager.init({
+      node: container,
+      flags: {
+        api: options.api,
+        thumbnailsUrl: options.thumbnailsUrl,
+        jwtToken: options.jwtToken || "",
+        dir: options.dir || "/"
+      }
+    });
+    fm.ports.upload.subscribe(upload);
+    fm.ports.download.subscribe(download);
+    return {
+      open: function open() {
+        fm.ports.onOpen.send(null);
+      },
 
-        fm.ports.upload.subscribe(upload);
-        fm.ports.download.subscribe(download);
+      set onClose(callback) {
+        fm.ports.close.subscribe(callback);
+      }
 
-        return {
-            open() {
-                fm.ports.onOpen.send(null);
-            },
-            set onClose(callback) { 
-                fm.ports.close.subscribe(callback);
-            }
-        };
     };
-})();
+  };
+}();
